3b356f53c3bbacb47712648af69fd3846bfc2872
datovarbol <datovarbol@gmail.com>
2024-05-31 19:09:06 -0500

    #

---
+++ prueba.js
@@ -0,0 +1,8 @@
+const textCali = document.querySelector("#miCiudad")
+const boton1 = document.querySelector("#boton1")
+boton1.onclick = clckFunction;
+//textCali.innerText += " y del deporte"
+function clckFunction(){
+    textCali.innerText+=" y del deporte";
+}
+
